2f9ee493122a44ec362d772666523b61c1fbd3d7
ArpitGoenka <2001arpitgoenka@gmail.com>
2024-05-18 12:10:36 +0300

    Ping_pong

--- Ping_Pong/paddle.py
+++ Ping_Pong/paddle.py
@@ -3,7 +3,7 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self,position):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.color("white")
@@ -12,9 +12,9 @@ class Paddle(Turtle):
         self.goto(position)
 
     def go_up(self):
-        y_cor= self.ycor() + 20
-        self.goto(self.xcor(),y_cor)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        y_cor = self.ycor() - 20
-        self.goto(self.xcor(), y_cor)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
