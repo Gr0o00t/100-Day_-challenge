04a2a855620f6a42dce25f8c921c9531e02ef2c7
ArpitGoenka <2001arpitgoenka@gmail.com>
2024-05-19 15:28:18 +0300

    turtle crossing

--- Turtle_Crossing/scoreboard.py
+++ Turtle_Crossing/scoreboard.py
@@ -11,12 +11,12 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(-250,250)
         self.hideturtle()
-        self.clear()
         self.update_score()
 
     def update_score(self):
+        self.clear()
         self.write(f"Level: {self.score}", align=ALIGNMENT, font=FONT)
 
     def Game_Over(self):
-
+        self.goto(0,0)
         self.write("GAME OVER", align=ALIGNMENT, font=FONT)
