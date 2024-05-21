fc072c0bcc44fe4c42396c03a180dba976896bec
ArpitGoenka <2001arpitgoenka@gmail.com>
2024-05-22 01:02:46 +0300

    Highest Score in Snake

---
+++ Mail_merge/main.py
@@ -0,0 +1,10 @@
+with open("./Input/Letters/starting_letter.txt", mode='r') as starting_letter:
+    letter = starting_letter.read()
+
+with open("./Input/Names/invited_names.txt") as invited_names:
+    names = invited_names.readlines()
+    for name in names:
+        stripped=name.strip()
+        letter = letter.replace("[name]", stripped)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped}.txt", mode='w') as file:
+            file.write(letter)
