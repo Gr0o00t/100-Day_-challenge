499824165742e49ebcaeab1b1a12e6ec408da817
ArpitGoenka <2001arpitgoenka@gmail.com>
2024-05-23 01:12:16 +0300

    Pandas

---
+++ Starting with Pandas/ Squirrel Census Data Analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey = len(data[data["Primary Fur Color"] == "Gray"] )
+cinnamon = len(data[data["Primary Fur Color"] == "Cinnamon"] )
+Black = len(data[data["Primary Fur Color"] == "Black"] )
+
+dict = {
+    "Primary Fur Color" :  ['Gray', 'Cinnamon', 'Black'],
+    "count" : [grey,cinnamon,Black]
+}
+
+df=pd.DataFrame(dict)
+df.to_csv("Squirral_count.csv")
+
