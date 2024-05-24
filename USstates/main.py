71134e691dc0a585f5196313ecde51ed92d0d89a
ArpitGoenka <2001arpitgoenka@gmail.com>
2024-05-24 22:12:03 +0300

    Guess States

--- USstates/main.py
+++ USstates/main.py
@@ -4,14 +4,20 @@ import pandas as pd
 
 turtle=Turtle()
 screen=Screen()
-stat_data= pd.read_csv("50_states.csv")
+data= pd.read_csv("50_states.csv")
 screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 
 gussed_answer=[]
 while len(gussed_answer)<50:
     answer = screen.textinput(title="U.S. State Guess Game", prompt="Guess a state name:" )
-    if answer not in gussed_answer:
-
+    print(answer)
+    if answer not in gussed_answer and answer in data.state:
+        gussed_answer.append(answer)
+        turtle.hideturtle()
+        turtle.penup()
+        turtle.goto(int.data[data.state == answer].x,int.data[data.state == answer].y)
+        turtle.write(answer)
+        print()
 
 screen.exitonclick()
