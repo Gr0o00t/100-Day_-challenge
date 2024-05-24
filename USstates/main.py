3fcb7382c5c775b6bd2a2e866e14812c62664aff
ArpitGoenka <2001arpitgoenka@gmail.com>
2024-05-24 22:13:45 +0300

    Guess States final changes

--- USstates/main.py
+++ USstates/main.py
@@ -5,7 +5,7 @@ screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-turtle.shape(imag)
+turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
