6c21447c20d060607027bc0d6df8e320e44dde4e
ArpitGoenka <2001arpitgoenka@gmail.com>
2024-05-28 22:59:18 +0300

    Phonetic_final push

--- NATO_alphabet/main.py
+++ NATO_alphabet/main.py
@@ -6,6 +6,6 @@ aplha = pd.read_csv("nato_phonetic_alphabet.csv")
 returna = { rows.letter:rows.code for (index,rows) in aplha.iterrows()}
 
 
-output = [ returna[lette] for lette in name]
+output1 = [ returna[lette] for lette in name]
 
 print (output)
